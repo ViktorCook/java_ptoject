8ac97be547791240e839b4f28eb3f84c4c33c51c
ViktorCook <92058663+ViktorCook@users.noreply.github.com>
2021-11-03 10:59:54 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,47 +1,64 @@
 
 
-let x1 = 2;
-let y1 = 3;
+// let x1 = 2;
+// let y1 = 3;
 
-let x2 = 10;
-let y2 = 5;
+// let x2 = 10;
+// let y2 = 5;
 
-let cathetus1 = Math.abs(x1 - x2);
+// let cathetus1 = Math.abs(x1 - x2);
 
-let cathetus2 = Math.abs(y1 - y2);
+// let cathetus2 = Math.abs(y1 - y2);
 
-console.log(Math.sqrt(
-    Math.pow(cathetus1, 2) * Math.pow(cathetus2, 2)
-  ));
+// console.log(Math.sqrt(
+//     Math.pow(cathetus1, 2) * Math.pow(cathetus2, 2)
+//   ));
 
 
-  let a = 13.890123;
-  let b = 2.891564;
-  let three = 3;
+//   let a = 13.890123;
+//   let b = 2.891564;
+//   let three = 3;
 
-  let aFloor = Math.floor(a);
-  let bFloor = Math.floor(b);
+//   let aFloor = Math.floor(a);
+//   let bFloor = Math.floor(b);
 
-  let aFloorNormalized = Math.round(
-    aFloor * Math.pow(10, three)
-    );
-    let bFloorNormalized = Math.round(
-    bFloor * Math.pow(10, three)
-    );
-    console.log(a, b);
-    console.log('a > b', aFloorNormalized > bFloorNormalized);
-    console.log('a < b', aFloorNormalized < bFloorNormalized);
-    console.log('a >= b', aFloorNormalized >= bFloorNormalized);
-    console.log('a <= b', aFloorNormalized <= bFloorNormalized);
-    console.log('a === b', aFloorNormalized === bFloorNormalized);
-    console.log('a ≠ b', aFloorNormalized !== bFloorNormalized);
+//   let aFloorNormalized = Math.round(
+//     aFloor * Math.pow(10, three)
+//     );
+//     let bFloorNormalized = Math.round(
+//     bFloor * Math.pow(10, three)
+//     );
+//     console.log(a, b);
+//     console.log('a > b', aFloorNormalized > bFloorNormalized);
+//     console.log('a < b', aFloorNormalized < bFloorNormalized);
+//     console.log('a >= b', aFloorNormalized >= bFloorNormalized);
+//     console.log('a <= b', aFloorNormalized <= bFloorNormalized);
+//     console.log('a === b', aFloorNormalized === bFloorNormalized);
+//     console.log('a ≠ b', aFloorNormalized !== bFloorNormalized);
 
-let m = -5;
-let n = 100;
-let range = Math.abs(Math.max(m, n)-(Math.min(m,n)));
-let Min = Math.min(m, n);
+// let m = -5;
+// let n = 100;
+// let range = Math.abs(Math.max(m, n)-(Math.min(m,n)));
+// let Min = Math.min(m, n);
  
-let firstRandomNumber = (Math.round(Math.random() * (range-2))) + Min;
-let FinalRandomResult = firstRandomNumber + (firstRandomNumber % 2) + 1;
+// let firstRandomNumber = (Math.round(Math.random() * (range-2))) + Min;
+// let FinalRandomResult = firstRandomNumber + (firstRandomNumber % 2) + 1;
  
-console.log(m, n, FinalRandomResult);
+// console.log(m, n, FinalRandomResult);
+
+let password = 'qwerty_-'
+
+if ( password.length > 4 && ( password.includes('_') || password.includes('-') ) ) {
+  console.log("Пароль надёжный")
+  }
+  else { console.log("Пароль недостаточно надёжный") }
+
+let name = 'viKtor';
+let surname = 'sMirnOv';
+
+let correctName = name.substr(0, 1).toUpperCase() + name.substr(1).toLowerCase();
+let correctSurname = surname.substr(0, 1).toUpperCase() + surname.substr(1).toLowerCase();
+
+let resultName = correctName != name || correctSurname != surname ? 'Имя было преобразовано' : 'Имя осталось без изменений';
+
+console.log(correctName, correctSurname, resultName)
